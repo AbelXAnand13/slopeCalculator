bf35beb81b4000c949dc62df59fccc3d9798ea84
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-01-17 19:54:07 -0800

    commit2 I might need to commit again, i was asked to create a line struct, need more info on that

--- slopeOfALine/main.cpp
+++ slopeOfALine/main.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-    
-float slopeCalcuator(int x1, int y1, int x2, int y2) {
-    float f1 = y2-y1;
-    float f2 = x2-x1;
-    
-    float slope = f2/f1;
-    return slope;
-}
 
 struct points {
     float x;
     float y;
 };
+
+float slopeCalcuator(points one, points two) {
+    float f1 = two.y - one.y;
+    float f2 = two.x - one.x;
+    
+    float slope = f2/f1;
+    return slope;
+}
 
 int main() {
     points points1;
@@ -28,9 +28,10 @@ int main() {
     cout << "Enter the second y value\n";
     cin >> points2.y;
     if (points1.x != points2.x) {
-    cout << "Slope = " << slopeCalcuator(points1.x, points1.y, points2.x, points2.y);
+    cout << "Slope = " << slopeCalcuator(points1, points2);
     }
     else {
         cout << "Undefined slope or same points\n";
     }
 }
+
